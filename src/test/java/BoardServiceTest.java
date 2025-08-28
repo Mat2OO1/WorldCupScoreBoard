765bfe024648a4bdc499176bf2898d67fa1f6f4a
Mat2OO1 <mateuszt.krajewski@gmail.com>
2025-08-28 14:46:51 +0200

    Add test case for starting game

---
+++ src/test/java/BoardServiceTest.java
@@ -0,0 +1,18 @@
+
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class BoardServiceTest {
+
+  @Test
+  public void shouldStartGame(){
+    BoardService boardService = new BoardService();
+
+    boardService.startGame("Home", "Away");
+
+    assertFalse(boardService.getGames().isEmpty());
+  }
+
+}
