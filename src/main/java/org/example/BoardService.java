7f7d903b4df3ae24e39493b87c8e7b4718f27f1d
Mat2OO1 <mateuszt.krajewski@gmail.com>
2025-08-28 15:06:45 +0200

    Handle existing game case

--- src/main/java/org/example/BoardService.java
+++ src/main/java/org/example/BoardService.java
@@ -3,6 +3,7 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import org.example.exception.GameAlreadyStartedException;
 import org.example.exception.GameNotFoundException;
 
 public class BoardService implements IBoardService {
@@ -10,19 +11,25 @@ public class BoardService implements IBoardService {
   private final List<Game> currentGames = new ArrayList<>();
 
   public void startGame(String homeTeam, String awayTeam) {
-    this.currentGames.add(Game.builder()
-        .homeTeam(homeTeam)
-        .awayTeam(awayTeam)
-        .homeGoals(0)
-        .awayGoals(0)
-        .build());
+    if (currentGames.stream().anyMatch(
+        game -> game.getHomeTeam().equals(homeTeam) && game.getAwayTeam().equals(awayTeam))) {
+      throw new GameAlreadyStartedException("Game already started");
+    } else {
+      this.currentGames.add(Game.builder()
+          .homeTeam(homeTeam)
+          .awayTeam(awayTeam)
+          .homeGoals(0)
+          .awayGoals(0)
+          .build());
+    }
   }
 
   public void finishGame(String homeTeam, String awayTeam) {
     Game foundGame = currentGames.stream()
         .filter(game -> game.getHomeTeam().equals(homeTeam) && game.getAwayTeam().equals(awayTeam))
         .findFirst()
-        .orElseThrow(() -> new GameNotFoundException("Game with given home team and away team not found"));
+        .orElseThrow(
+            () -> new GameNotFoundException("Game with given home team and away team not found"));
     currentGames.remove(foundGame);
   }
 
