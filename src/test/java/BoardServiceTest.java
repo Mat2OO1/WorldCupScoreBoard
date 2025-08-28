906cd4a953c2503677177646f63e473556b4f2a8
Mat2OO1 <mateuszt.krajewski@gmail.com>
2025-08-28 14:52:39 +0200

    Add test for finishing game

--- src/test/java/BoardServiceTest.java
+++ src/test/java/BoardServiceTest.java
@@ -1,6 +1,7 @@
 
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.example.BoardService;
 import org.junit.Test;
@@ -8,12 +9,22 @@ import org.junit.Test;
 public class BoardServiceTest {
 
   @Test
-  public void shouldStartGame(){
+  public void shouldStartGame() {
     BoardService boardService = new BoardService();
 
     boardService.startGame("Home", "Away");
 
     assertFalse(boardService.getGames().isEmpty());
+  }
+
+  @Test
+  public void shouldFinishGame() {
+    BoardService boardService = new BoardService();
+
+    boardService.startGame("Home", "Away");
+    boardService.finishGame("Home", "Away");
+
+    assertTrue(boardService.getGames().isEmpty());
   }
 
 }
