4a6ae98d116386d1b1b38ec821dde86c45b427ce
Mat2OO1 <mateuszt.krajewski@gmail.com>
2025-08-29 10:00:16 +0200

    update junit4 to junit5

--- src/test/java/BoardServiceTest.java
+++ src/test/java/BoardServiceTest.java
@@ -1,14 +1,14 @@
 
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.example.BoardService;
 import org.example.exception.GameAlreadyStartedException;
 import org.example.exception.GameNotFoundException;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class BoardServiceTest {
 
