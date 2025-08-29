a59a3240e1624b4701ec9f7f76cb5b1cc67c4fd7
Mat2OO1 <mateuszt.krajewski@gmail.com>
2025-08-29 10:30:54 +0200

    adjust tests to handle game repository related logic

--- src/test/java/BoardServiceTest.java
+++ src/test/java/BoardServiceTest.java
@@ -6,65 +6,66 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.example.BoardService;
+import org.example.GameRepository;
 import org.example.exception.GameAlreadyStartedException;
 import org.example.exception.GameInputValidationException;
 import org.example.exception.GameNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class BoardServiceTest {
 
+  private GameRepository gameRepository;
+  private BoardService boardService;
+
+  @BeforeEach
+  public void setUp() {
+    gameRepository = new GameRepository();
+    boardService = new BoardService(gameRepository);
+  }
+
   @Test
   public void shouldStartGame() {
-    BoardService boardService = new BoardService();
-
     boardService.startGame("Home", "Away");
 
-    assertFalse(boardService.getGames().isEmpty());
+    assertFalse(gameRepository.getGames().isEmpty());
   }
 
   @ParameterizedTest
-  @CsvSource(value = {"null, away", "home, null", ",away", "home,", "home,' '"}, nullValues = "null")
+  @CsvSource(value = {"null, away", "home, null", ",away", "home,",
+      "home,' '"}, nullValues = "null")
   public void shouldNotStartGameWhenInputIsInvalid(String homeTeam, String awayTeam) {
-    BoardService boardService = new BoardService();
-
-    assertThrows(
-        GameInputValidationException.class, () -> boardService.startGame(homeTeam, awayTeam));
+    assertThrows(GameInputValidationException.class,
+        () -> boardService.startGame(homeTeam, awayTeam));
   }
 
   @Test
   public void shouldThrowExceptionWhenGameAlreadyStarted() {
-    BoardService boardService = new BoardService();
     boardService.startGame("Home", "Away");
 
     assertThrows(GameAlreadyStartedException.class, () -> boardService.startGame("Home", "Away"));
-
   }
 
   @Test
   public void shouldFinishGame() {
-    BoardService boardService = new BoardService();
-
     boardService.startGame("Home", "Away");
     boardService.finishGame("Home", "Away");
 
-    assertTrue(boardService.getGames().isEmpty());
+    assertTrue(gameRepository.getGames().isEmpty());
   }
 
   @ParameterizedTest
-  @CsvSource(value = {"null, away", "home, null", ",away", "home,", "' ',away"}, nullValues = "null")
+  @CsvSource(value = {"null, away", "home, null", ",away", "home,",
+      "' ',away"}, nullValues = "null")
   public void shouldNotFinishGameWhenInputIsInvalid(String homeTeam, String awayTeam) {
-    BoardService boardService = new BoardService();
-
-    assertThrows(
-        GameInputValidationException.class, () -> boardService.finishGame(homeTeam, awayTeam));
+    assertThrows(GameInputValidationException.class,
+        () -> boardService.finishGame(homeTeam, awayTeam));
   }
 
   @Test
   public void shouldThrowExceptionWhenGameToFinishNotFound() {
-    BoardService boardService = new BoardService();
-
     boardService.startGame("Home", "Away");
 
     assertThrows(GameNotFoundException.class, () -> boardService.finishGame("abc", "abc"));
@@ -72,12 +73,11 @@ public class BoardServiceTest {
 
   @Test
   public void shouldUpdateScore() {
-    BoardService boardService = new BoardService();
     boardService.startGame("Home", "Away");
 
     boardService.updateScore("Home", "Away", 1, 3);
 
-    var updatedGame = boardService.getGames().getFirst();
+    var updatedGame = gameRepository.getGames().getFirst();
     assertEquals(1, updatedGame.getHomeGoals());
     assertEquals(3, updatedGame.getAwayGoals());
   }
@@ -85,33 +85,24 @@ public class BoardServiceTest {
   @ParameterizedTest
   @CsvSource(value = {"null, away", "home, null", ",away", "home,"}, nullValues = "null")
   public void shouldNotUpdateScoreWhenTeamInputIsInvalid(String homeTeam, String awayTeam) {
-    BoardService boardService = new BoardService();
-
-    assertThrows(
-        GameInputValidationException.class,
+    assertThrows(GameInputValidationException.class,
         () -> boardService.updateScore(homeTeam, awayTeam, 1, 1));
   }
 
   @ParameterizedTest
   @CsvSource(value = {"1,-2", "-2,1"})
   public void shouldNotUpdateScoreWhenScoreInputIsInvalid(int homeScore, int awayScore) {
-    BoardService boardService = new BoardService();
-
-    assertThrows(
-        GameInputValidationException.class,
+    assertThrows(GameInputValidationException.class,
         () -> boardService.updateScore("home", "away", homeScore, awayScore));
   }
 
   @Test
   public void shouldThrowExceptionWhenGameToUpdateScoreNotFound() {
-    BoardService boardService = new BoardService();
-
     assertThrows(GameNotFoundException.class, () -> boardService.updateScore("Home", "Away", 1, 3));
   }
 
   @Test
   public void shouldReturnSummaryOfGames() {
-    BoardService boardService = new BoardService();
     boardService.startGame("Spain", "Brazil");
     boardService.startGame("Germany", "France");
     boardService.startGame("Argentina", "Australia");
@@ -129,5 +120,4 @@ public class BoardServiceTest {
     assertEquals("Germany", summary.get(2).getHomeTeam());
     assertEquals("France", summary.get(2).getAwayTeam());
   }
-
 }
