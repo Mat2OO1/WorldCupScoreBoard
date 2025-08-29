dfac44c5cc101e195e7c1ee76d89259cc0bd2b04
Mat2OO1 <mateuszt.krajewski@gmail.com>
2025-08-29 08:58:35 +0200

    refactor boardService class

--- src/main/java/org/example/BoardService.java
+++ src/main/java/org/example/BoardService.java
@@ -12,28 +12,26 @@ public class BoardService implements IBoardService {
   private final List<Game> currentGames = new ArrayList<>();
 
   public void startGame(String homeTeam, String awayTeam) {
-    if (this.getGame(homeTeam, awayTeam).isPresent()) {
+    this.getGame(homeTeam, awayTeam).ifPresent(game -> {
       throw new GameAlreadyStartedException(BoardConstants.GAME_ALREADY_STARTED);
-    } else {
-      this.currentGames.add(
-          Game.builder()
-              .homeTeam(homeTeam)
-              .awayTeam(awayTeam)
-              .homeGoals(BoardConstants.GAME_STARTING_VALUE)
-              .awayGoals(BoardConstants.GAME_STARTING_VALUE)
-              .build());
-    }
+    });
+    this.currentGames.add(Game.builder()
+        .homeTeam(homeTeam)
+        .awayTeam(awayTeam)
+        .homeGoals(BoardConstants.GAME_STARTING_VALUE)
+        .awayGoals(BoardConstants.GAME_STARTING_VALUE)
+        .build());
   }
 
   public void finishGame(String homeTeam, String awayTeam) {
-    Game foundGame = this.getGame(homeTeam, awayTeam).orElseThrow(
-        () -> new GameNotFoundException(BoardConstants.GAME_NOT_FOUND));
-    currentGames.remove(foundGame);
+    this.getGame(homeTeam, awayTeam).ifPresentOrElse(currentGames::remove, () -> {
+      throw new GameNotFoundException(BoardConstants.GAME_NOT_FOUND);
+    });
   }
 
   public Game updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore) {
-    Game foundGame = this.getGame(homeTeam, awayTeam).orElseThrow(
-        () -> new GameNotFoundException(BoardConstants.GAME_NOT_FOUND));
+    Game foundGame = this.getGame(homeTeam, awayTeam)
+        .orElseThrow(() -> new GameNotFoundException(BoardConstants.GAME_NOT_FOUND));
     foundGame.setHomeGoals(homeScore);
     foundGame.setAwayGoals(awayScore);
     return foundGame;
